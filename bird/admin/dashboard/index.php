372c85069c7f4fd098ad204c13be8393cf15fceb
jassim391 <jassim.alhurumi09@gmail.com>
2025-06-03 23:58:06 +0300

    Update index.php

--- bird/admin/dashboard/index.php
+++ bird/admin/dashboard/index.php
@@ -510,68 +510,15 @@ h1, .h1 {
 <body>
 
 <div class='dashboard'>
-  <div class="dashboard-nav">
-    <header>
-      <a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a>
-      <a href="#" class="brand-logo"><i class="fas fa-anchor"></i> <span>BRAND</span></a>
-    </header>
-    <nav class="dashboard-nav-list">
-      <a href="#" class="dashboard-nav-item"><i class="fas fa-home"></i> Home</a>
-      <a href="#" class="dashboard-nav-item active"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
-     <div class='dashboard-nav-dropdown'>
-  <a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle">
-    <i class="fa-solid fa-box-open"></i> Products
-  </a>
-  <div class='dashboard-nav-dropdown-menu'>
-    <a href="products.php" class="dashboard-nav-dropdown-item">All</a>
-    <a href="products.php?category=1" class="dashboard-nav-dropdown-item">Accessories</a>
-    <a href="products.php?category=2" class="dashboard-nav-dropdown-item">Birds</a>
-    <a href="products.php?category=3" class="dashboard-nav-dropdown-item">Food</a>
-  </div>
-</div>
-      <a href="#" class="dashboard-nav-item"><i class="fas fa-file-upload"></i> Upload</a>
-      <div class='dashboard-nav-dropdown'>
-        <a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-photo-video"></i> Media</a>
-        <div class='dashboard-nav-dropdown-menu'>
-          <a href="#" class="dashboard-nav-dropdown-item">All</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Recent</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Images</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Video</a>
-        </div>
-      </div>
-      <div class='dashboard-nav-dropdown'>
-        <a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-users"></i> Users</a>
-        <div class='dashboard-nav-dropdown-menu'>
-          <a href="#" class="dashboard-nav-dropdown-item">All</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Subscribed</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Non-subscribed</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Banned</a>
-          <a href="#" class="dashboard-nav-dropdown-item">New</a>
-        </div>
-      </div>
-      <div class='dashboard-nav-dropdown'>
-        <a href="#!" class="dashboard-nav-item dashboard-nav-dropdown-toggle"><i class="fas fa-money-check-alt"></i> Payments</a>
-        <div class='dashboard-nav-dropdown-menu'>
-          <a href="#" class="dashboard-nav-dropdown-item">All</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Recent</a>
-          <a href="#" class="dashboard-nav-dropdown-item">Projections</a>
-        </div>
-      </div>
-      <a href="#" class="dashboard-nav-item"><i class="fas fa-cogs"></i> Settings</a>
-      <a href="#" class="dashboard-nav-item"><i class="fas fa-user"></i> Profile</a>
-      <div class="nav-item-divider"></div>
-      <a  href="logout.php" class="dashboard-nav-item"><i class="fas fa-sign-out-alt"></i> Logout</a>
-    </nav>
-  </div>
 
+  <?php include '../../include/nav-bar.php';?>
+  
   <div class='dashboard-app'>
     <header class='dashboard-toolbar'><a href="#!" class="menu-toggle"><i class="fas fa-bars"></i></a></header>
     <div class='dashboard-content'>
-      <div class='container'>
-
         <div class='card mb-4'>
           <div class='card-header'>
-    <h1>Wellcome Back : <?php echo htmlspecialchars($admin_name); ?> </h1>
+          <h1>Wellcome Back : <?php echo htmlspecialchars($admin_name); ?> </h1>
           </div>
           <div class='card-body'>
             <p>Your account type is: Administrator</p>
@@ -632,8 +579,6 @@ h1, .h1 {
             </table>
           </div>
         </div>
-
-      </div>
     </div>
   </div>
 </div>
